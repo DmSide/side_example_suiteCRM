93815c1f60a86a1d2ffad6a503192bc5053565fb
Dmitriy Sidenko <dmsidenko@gmail.com>
2019-03-27 00:57:34 +0300

    Bad version

---
+++ custom/modules/Opportunities/language/ru_RU.lang.php
@@ -0,0 +1,8 @@
+<?php
+// created: 2019-03-26 16:52:02
+$mod_strings = array (
+  'LBL_DATE_ENTERED' => 'Дата создания:',
+  'LBL_ENGINEER_USER_ID' => '&#039;engineer&#039; (связано с ID модуля &#039;Пользователь&#039;)',
+  'LBL_ENGINEER' => 'Инженер',
+  'LBL_EDITVIEW_PANEL1' => 'Новая панель 1',
+);
