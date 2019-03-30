67ebcc4867dae19a4e5fb13bebc7748d55e7d2c6
Dmitriy Sidenko <dmsidenko@gmail.com>
2019-03-31 01:07:58 +0300

    Work all process of creating tasks

--- custom/modules/Opportunities/hooks/create_new_task_hook.php
+++ custom/modules/Opportunities/hooks/create_new_task_hook.php
@@ -14,7 +14,7 @@ class create_new_task_hook
 
 //        'Primary contact' => 'Первичный контакт',
 //        'Technical department' => 'Техотдел',
-//        'Office' => 'Офис',descr
+//        'Office' => 'Офис',
 //        'Qualification' => 'Оценка',
 //        'Matching' => 'Согласование',
 //        'Price Quote' => 'Счет',
@@ -24,6 +24,9 @@ class create_new_task_hook
 //        'Closed Lost' => 'Закрыто с потерей',
 //
         $GLOBALS['log']->debug('Start create_new_task_hook');
+        if ($bean->sales_stage == "Closed Won" or $bean->sales_stage == "Закрыто с успехом") return;
+        if ($bean->sales_stage == "Primary contact" or $bean->sales_stage == "Первичный контакт") return;
+
         $now = date_create()->format('Y-m-d H:i:s');
         $uuid = sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
             // 32 bits for "time_low"
@@ -40,6 +43,7 @@ class create_new_task_hook
             // 48 bits for "node"
             mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
         );
+
         //            $task = new Task();
         //            $task->id = $uuid;
         //            $task->created_by = '1';
@@ -56,28 +60,81 @@ class create_new_task_hook
         //            $GLOBALS['log']->debug($now);
         //            $GLOBALS['log']->debug($tomorrow);
         //            $GLOBALS['log']->debug($uuid);
-        $name = 'SIDE test';
-        $description = '';
-        $created_by = $bean->created_by;
-        $assigned_user_list = array(
-            'techotdel' => 'c6c52b5c-cbed-5bbb-8d58-5c5bfb65720f',
-            'office' => '',
-            'denis' => '570cfb3e-fe1f-ed10-68f4-5c5bfaa04842',
-            'manager' => $created_by,
-
-        );
-        $GLOBALS['log']->debug($assigned_user_list['techotdel']);
-        $GLOBALS['log']->debug($bean->sales_stage);
-        $date_due_flag = ($bean->sales_stage == "Price Quote" or $bean->sales_stage == "Счет")
+        //***************Get contact_id****************************
+        $dbGetContactId = DBManagerFactory::getInstance();
+        $queryGetContactId = "SELECT ac.contact_id as contact_id
+                             FROM accounts_contacts ac, accounts_opportunities ao
+                             WHERE ac.deleted =0 and ao.deleted = 0
+                             and ac.account_id = ao.account_id 
+                             and ao.opportunity_id = '{$bean->id}'";
+        $resultGetContactId = $dbGetContactId->query($queryGetContactId);
+        $resultGetContactIdRow = $dbGetContactId->fetchByAssoc($resultGetContactId);
+        $contact_id  = $resultGetContactIdRow['contact_id'];
+        //***************Get $deal_manager_c************************
+        $dbGetManager = DBManagerFactory::getInstance();
+        $queryGetManager = "SELECT deal_manager_c FROM opportunities_cstm WHERE id_c = '{$bean->id}'";
+        $resultGetManager = $dbGetManager->query($queryGetManager);
+        $resultGetManagerRow = $dbGetManager->fetchByAssoc($resultGetManager);
+        $deal_manager_c = $resultGetManagerRow['deal_manager_c'];
+        //*****************Get $date_due*****************************
+        $date_due = ($bean->sales_stage == "Price Quote" or $bean->sales_stage == "Счет")
             ? date('Y-m-d H:i:s', time() + 86400*3)
             : date('Y-m-d H:i:s', time() + 86400); //$tomorrow;
-        $assigned_user_id = $assigned_user_list['techotdel'];
+        //***************Create list of users***********************
+        $assigned_user_list = array(
+            'techotdel' => 'c6c52b5c-cbed-5bbb-8d58-5c5bfb65720f',
+            'office' => '72ddba62-cc55-edb2-3260-5c9fcf714535',
+            'denis' => '570cfb3e-fe1f-ed10-68f4-5c5bfaa04842',
+            'manager' => $deal_manager_c,
+        );
+        //*********Fill up field $assigned_user_id and $name*************************************************
         if ($bean->sales_stage == "Technical department" or $bean->sales_stage == "Техотдел") {
-            $dbInsertNewTask = DBManagerFactory::getInstance();
-            $queryInsertNewTask = "INSERT INTO tasks (id, name, created_by, assigned_user_id, status, deleted,  date_start, date_entered, date_due_flag, date_due, description, parent_type, parent_id)
-                                VALUES ('{$uuid}', '{$name}', '{$created_by}', '{$assigned_user_id}','Not Started', 0,'{$now}' ,'{$now}',0,'{$date_due_flag}','{$bean->description}','Opportunities', '{$bean->id}')";
-            $dbInsertNewTask->query($queryInsertNewTask);
+            $assigned_user_id = $assigned_user_list['techotdel'];
+            $name = 'Выбрать инженера';
+        } else if ($bean->sales_stage == "Office" or $bean->sales_stage == "Офис") {
+            $assigned_user_id = $assigned_user_list['office'];
+            $name = 'Загрузить акт';
+        }else if ($bean->sales_stage == "Qualification" or $bean->sales_stage == "Оценка") {
+            $assigned_user_id = $assigned_user_list['denis'];
+            $name = 'Оценить';
         }
+        else if ($bean->sales_stage == "Matching" or $bean->sales_stage == "Согласование") {
+            $assigned_user_id = $assigned_user_list['manager'];
+            $name = 'Согласовать';
+        }
+        else if ($bean->sales_stage == "Price Quote" or $bean->sales_stage == "Счет") {
+            $assigned_user_id = $assigned_user_list['manager'];
+            $name = 'Контроль оплаты';
+        }else if ($bean->sales_stage == "Order" or $bean->sales_stage == "Заказ") {
+            $assigned_user_id = $assigned_user_list['denis'];
+            $name = 'Заказать';
+        }else if ($bean->sales_stage == "Waiting" or $bean->sales_stage == "Ожидание товара") {
+            $assigned_user_id = $assigned_user_list['denis'];
+            $name = 'Поступление';
+        }
+        else if ($bean->sales_stage == "Sale" or $bean->sales_stage == "Реализация") {
+            $assigned_user_id = $assigned_user_list['techotdel'];
+            $name = 'Реализовать';
+        }else if ($bean->sales_stage == "Closed Lost" or $bean->sales_stage == "Закрыто с потерей") {
+            $assigned_user_id = $assigned_user_list['office'];
+            $name = 'Счет на диагностику';
+        }
+
+        $GLOBALS['log']->debug('*************PARAGUE***************');
+        $GLOBALS['log']->debug($assigned_user_list['techotdel']);
+        $GLOBALS['log']->debug($bean->sales_stage);
+
+        $dbInsertNewTask = DBManagerFactory::getInstance();
+        $queryInsertNewTask = "INSERT INTO tasks (
+                                    id, name, created_by, assigned_user_id, status, deleted, description, 
+                                    parent_type, parent_id, priority, contact_id,
+                                    date_start, date_entered, date_due_flag, date_due
+                                ) VALUES (
+                                    '{$uuid}', '{$name}', '{$bean->created_by}', '{$assigned_user_id}','Not Started', 0,'{$bean->description}',
+                                    'Opportunities', '{$bean->id}', 'Medium', '{$contact_id}',
+                                    '{$now}' ,'{$now}',0,'{$date_due}'
+                                )";
+        $dbInsertNewTask->query($queryInsertNewTask);
         $GLOBALS['log']->debug('Finish create_new_task_hook');
     }
 
