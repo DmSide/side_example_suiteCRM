086f5ab8f50d3a94959cf40645e0b9ca1bf6492d
Dmitriy Sidenko <dmsidenko@gmail.com>
2019-03-31 07:29:45 +0300

    Cut debug info

--- custom/modules/Opportunities/hooks/make_tasks_completed_hook.php
+++ custom/modules/Opportunities/hooks/make_tasks_completed_hook.php
@@ -13,9 +13,11 @@
     {
         function main(&$bean, $event, $arguments)
         {
+            $GLOBALS['log']->debug('Start make_tasks_completed_hook');
             $dbUpdateTasks = DBManagerFactory::getInstance();
             $queryUpdateTasks = "UPDATE tasks SET status = 'Completed' WHERE parent_id ='{$bean->id}' and deleted = 0";
             $dbUpdateTasks->query($queryUpdateTasks);
+            $GLOBALS['log']->debug('Stop make_tasks_completed_hook');
         }
     }
 ?>
--- custom/modules/Opportunities/hooks/update_account_fileds_hook.php
+++ custom/modules/Opportunities/hooks/update_account_fileds_hook.php
@@ -12,31 +12,17 @@ class update_account_fileds_hook
 {
     function main(&$bean, $event, $arguments){
         $GLOBALS['log']->debug('Start update_account_fileds_hook');
-        $GLOBALS['log']->debug($event);
-        $GLOBALS['log']->debug($arguments);
-
         #FILL UP deal_address_c
         $dbSelectAccountFileds = DBManagerFactory::getInstance();
         //deal_creator_c = '',  engineer_c = ''
         $queryAccountFileds = "SELECT a.billing_address_street as street, a.id as id 
                                    FROM accounts a, accounts_opportunities ao
                                    WHERE a.deleted = 0 and ao.account_id = a.id and ao.opportunity_id ='{$bean->id}' LIMIT 1";
-        $GLOBALS['log']->debug('Debugging message 1');
-        $resultAccount = $dbSelectAccountFileds->query($queryAccountFileds) or die(mysql_error());
-        $GLOBALS['log']->debug('Debugging message 2');
-        #$resultAccountRow =mysqli_fetch_row($resultAccount);
-        #$resultAccountRow =mysqli_fetch_row($resultAccount);
-        #$resultAccountRow = mysql_fetch_array($resultAccount);
+        $resultAccount = $dbSelectAccountFileds->query($queryAccountFileds);
         $resultAccountRow = $dbSelectAccountFileds->fetchByAssoc($resultAccount);
-        $GLOBALS['log']->debug('Debugging message 2/2');
-        $GLOBALS['log']->debug($resultAccountRow);
         $result_account_id = $resultAccountRow['id'];
-        $GLOBALS['log']->debug('Debugging message 3');
         $result_street = $resultAccountRow['street'];
-        $GLOBALS['log']->debug('Debugging message 4');
-        $GLOBALS['log']->debug($result_account_id);
-        $GLOBALS['log']->debug($result_street);
-        $GLOBALS['log']->debug($bean->id);
+
         if (is_null($result_street) or $result_street == '') {
             $result_street = 'Нет данных';
         }
@@ -49,13 +35,9 @@ class update_account_fileds_hook
                   FROM accounts a, users u
                   WHERE a.deleted = 0 and u.deleted = 0 
                         and u.id = a.assigned_user_id  and a.id = '{$result_account_id}'";
-            $GLOBALS['log']->debug('Debugging message 6/1');
             $resultManager = $dbSelectManager->query($querySelectManager);
-            $GLOBALS['log']->debug('Debugging message 6/2');
             $resultManagerRow = $dbSelectManager->fetchByAssoc($resultManager);
-            $GLOBALS['log']->debug('Debugging message 6/3');
             $result_manager_name = $resultManagerRow['user_name'];
-            $GLOBALS['log']->debug('Debugging message 6/4');
             if (is_null($result_manager_name) or $result_manager_name == '') {
                 $result_manager_name = 'Нет данных';
             }
@@ -74,7 +56,7 @@ class update_account_fileds_hook
         }
 
         $dbUpdateOpportunities = DBManagerFactory::getInstance();
-        $queryUpdateOpportunities = "UPDATE opportunities 
+        $queryUpdateOpportunities = "UPDATE opportunities
                                     SET created_by_name_c = '{$result_created_by_name}', 
                                         deal_manager_c = '{$result_manager_name}',
                                         deal_address_c = '{$result_street}'
