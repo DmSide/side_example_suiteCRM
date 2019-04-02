c2954ebc211e010710ce4ee0f084225a6a56d0bd
Dmitriy Sidenko <dmsidenko@gmail.com>
2019-04-03 02:15:48 +0300

    Add custom Opportunities directory to git

---
+++ custom/modules/Opportunities/Ext/Language/ru_RU.lang.ext.php
@@ -0,0 +1,29 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+
+
+//THIS FILE IS AUTO GENERATED, DO NOT MODIFY
+$mod_strings['LBL_OPPORTUNITIES_DOCUMENTS_1_FROM_DOCUMENTS_TITLE'] = 'Документы';
+
+
+/**
+ * Created by PhpStorm.
+ * User: dima
+ * Date: 31.03.19
+ * Time: 2:10
+ */
+
+$mod_strings = array_merge($mod_strings,
+   array(
+       'LBL_GOOD_COST_C' => "Стоимость товара",
+       'LBL_GOOD_DATE_C' => "Сроки поставки",
+       'LBL_GOOD_DATE_COMMING_C' => 'Дата поступления',
+       'LBL_ENGINEER_C' => 'Инженер',
+       'LBL_DEAL_ADDRESS_C' => 'Адрес выезда',
+       'LBL_CREATED_BY_NAME_C' => 'Кем создано',
+       'LBL_DEAL_MANAGER_C' => 'Ответственный менеджер',
+       'LBL_LOADED_ACT_C' => 'Акт мастера',
+   )
+);
+
+?>
