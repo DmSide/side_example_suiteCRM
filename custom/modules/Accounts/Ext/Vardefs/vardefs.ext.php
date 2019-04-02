90d440d757a2a3843a3c2640461fa1aff71b2b5b
Dmitriy Sidenko <dmsidenko@gmail.com>
2019-04-03 01:58:12 +0300

    Remove account_opportunities_1

--- custom/modules/Accounts/Ext/Vardefs/vardefs.ext.php
+++ custom/modules/Accounts/Ext/Vardefs/vardefs.ext.php
@@ -12,18 +12,6 @@ $dictionary['Account']['fields']['jjwg_maps_lat_c']['inline_edit']=1;
 
  
 
-// created: 2019-03-29 02:18:08
-$dictionary["Account"]["fields"]["accounts_opportunities_1"] = array (
-  'name' => 'accounts_opportunities_1',
-  'type' => 'link',
-  'relationship' => 'accounts_opportunities_1',
-  'source' => 'non-db',
-  'module' => 'Opportunities',
-  'bean_name' => 'Opportunity',
-  'vname' => 'LBL_ACCOUNTS_OPPORTUNITIES_1_FROM_OPPORTUNITIES_TITLE',
-);
-
-
  // created: 2019-03-12 21:43:12
 $dictionary['Account']['fields']['jjwg_maps_address_c']['inline_edit']=1;
 
