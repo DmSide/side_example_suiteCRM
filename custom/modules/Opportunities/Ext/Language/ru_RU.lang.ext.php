4050085778fea084ff34c2a873791a0496936eae
Dmitriy Sidenko <dmsidenko@gmail.com>
2019-04-03 02:34:10 +0300

    Remove opportunities_documents_1

--- custom/modules/Opportunities/Ext/Language/ru_RU.lang.ext.php
+++ custom/modules/Opportunities/Ext/Language/ru_RU.lang.ext.php
@@ -2,10 +2,6 @@
  //WARNING: The contents of this file are auto-generated
 
 
-//THIS FILE IS AUTO GENERATED, DO NOT MODIFY
-$mod_strings['LBL_OPPORTUNITIES_DOCUMENTS_1_FROM_DOCUMENTS_TITLE'] = 'Документы';
-
-
 /**
  * Created by PhpStorm.
  * User: dima
--- custom/modules/Opportunities/Ext/Vardefs/vardefs.ext.php
+++ custom/modules/Opportunities/Ext/Vardefs/vardefs.ext.php
@@ -24,18 +24,6 @@ $dictionary['Opportunity']['fields']['jjwg_maps_lat_c']['inline_edit']=1;
 
  
 
-// created: 2019-03-29 01:45:25
-$dictionary["Opportunity"]["fields"]["opportunities_documents_1"] = array (
-  'name' => 'opportunities_documents_1',
-  'type' => 'link',
-  'relationship' => 'opportunities_documents_1',
-  'source' => 'non-db',
-  'module' => 'Documents',
-  'bean_name' => 'Document',
-  'vname' => 'LBL_OPPORTUNITIES_DOCUMENTS_1_FROM_DOCUMENTS_TITLE',
-);
-
-
  // created: 2019-03-30 04:11:03
 $dictionary['Opportunity']['fields']['date_closed']['display_default']='+1 day';
 $dictionary['Opportunity']['fields']['date_closed']['inline_edit']=true;
